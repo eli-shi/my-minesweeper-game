bc2749f8501ab86bb319415f69906c0ba01febe7
eli-shi <shirinyan.elina@gmail.com>
2025-08-22 15:57:38 +0200

    ref: moved css for the side banners into a seperate file

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,14 @@
 // import minePatch from "../assets/minePatch.png";
 // import mine from "../assets/mine.png";
-
+import "../css/banner.css";
 
 export const Banner = ({ side }: { side: string }) => {
     // const tiles = [minePatch, mine]
     return (
         <div className={`${side}`}>
+            <div className="animatedMinePatches">
 
+            </div>
         </div>
 
     )
