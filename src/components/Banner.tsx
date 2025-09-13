14a07c2faf339f24a093b3ec5f98d6e9de9250cd
eli-shi <shirinyan.elina@gmail.com>
2025-09-13 13:23:01 +0200

    style: added animated patches to the side banners

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,10 @@
-// import minePatch from "../assets/minePatch.png";
-// import mine from "../assets/mine.png";
 import "../css/banner.css";
 
 export const Banner = ({ side }: { side: string }) => {
-    // const tiles = [minePatch, mine]
     return (
         <div className={`${side}`}>
-            <div className="animatedMinePatches">
-
-            </div>
+            <div className="animatedMinePatches"></div>
+            <div className="animatedMinePatches"></div>
         </div>
 
     )
