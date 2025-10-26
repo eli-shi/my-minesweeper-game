755f669aa6d4b1170479357c41d644cf96ff4b3b
eli-shi <shirinyan.elina@gmail.com>
2025-10-26 13:04:04 +0100

    refactor: changed how the menu and minesweeper components render at the app.tsx level, changed useGameOver accordingly.
    WARNING: there are imports from files Login.tsx, Signup.tsx, AuthContext.tsx which are not part of the repo yet, relevent lines are commented to avoid issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './css/App.css'
 import { Minesweeper } from './components/Minesweeper.tsx'
 import { Menu } from './components/Menu.tsx'
 import './css/minesweeper.css'
 import { Banner } from './components/Banner.tsx'
+// import { Login } from './components/Login'
+// import { Signup } from './components/Signup'
+// import { AuthProvider, useAuth } from './context/AuthContext'
+import { useGameOver } from './hooks/useGameOver.tsx'
 
-function App() {
-  const [gameOver, setGameOver] = useState(true)
-
+function GameContent() {
 
   return (
-    <div>
-
-      {gameOver ? (
-        <div className="menu">
-          {/* <div className="rightBanner"></div> */}
-          <Banner side="rightBanner"></Banner>
-          <Menu resetGameOver={() => setGameOver(false)} />
-          <Banner side="leftBanner"></Banner>
-          {/* <div className="leftBanner"></div> */}
-        </div>
-      ) : (
-        <div className="minesweeper">
-          <Minesweeper rows={10} columns={10} numberOfMines={5} triggerGameOver={() => setGameOver(true)} />
-        </div>
-      )}
-
+    <div className="menu">
+      <Banner side="rightBanner" />
+      <Menu />
+      <Banner side="leftBanner" />
     </div>
+  )
+}
+
+function AppContent() {
+  // useGameOver returns [gameOver, triggerGameOver, resetGameOver]
+  // we need the second item (the function) to pass into Minesweeper
+  const [, triggerGameOver] = useGameOver();
+
+  return (
+    <Routes>
+      {/* <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} /> */}
+      <Route path="/" element={<GameContent />} />
+      <Route path="/game" element={<Minesweeper rows={10} columns={10} numberOfMines={5} triggerGameOver={triggerGameOver} />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      {/* <AuthProvider> */}
+      <AppContent />
+      {/* </AuthProvider> */}
+    </Router>
   )
 }
 
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export const Menu = ({ resetGameOver }: { resetGameOver: () => void }) => {
+export const Menu = () => {
 
     return (
         <div>
@@ -8,10 +8,12 @@ export const Menu = ({ resetGameOver }: { resetGameOver: () => void }) => {
 
 
 
-            <button onClick={() => {
+            {/* <button onClick={() => {
                 console.log("game reset button clicked");
                 resetGameOver();
-            }}>Play</button>
+            }}>Play</button> */}
+
+            <Link to="/game" >Play</Link>
             {/*play minesweeper, should be play as guest later once authorization/user accounts has been implemented*/}
             <a>Login</a>
             <a>Register</a>
