6048f5ce21e51000f7525de97b9b08860f2218c6
eli-shi <shirinyan.elina@gmail.com>
2025-11-12 15:36:55 +0100

    refactor: fixed the schema and created a seed file to create data for the database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,11 @@
+// // Load .env into process.env when running Prisma commands (migrations, seed, etc.)
+// import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+
+type Env = {
+  DATABASE_URL: string
+}
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
@@ -7,6 +13,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env<Env>('DATABASE_URL'),
   },
 });
